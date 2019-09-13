20ca05b229ec5fc34d58387da4ae164119fbfb41
Daniel (TMP) Seuffer <authanram@gmail.com>
2019-09-13 22:14:26 +0200

    Add Laravel. Add shared imprint and corresponding routes.

---
+++ resources/views/imprint.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Back soon.</title>
+    @include('styles')
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+
+        <div class="title m-b-md">
+            Imprint
+        </div>
+
+        <div class="m-b-md--small">
+            {{ request()->cookie('domain') ?? 'killsu.com' }}
+            <br>
+            <br>
+            Daniel Seuffer, Geißwiesen 24/1, D-72227 Egenhausen
+            <br>
+            d.seuffer@gmail.com | 0049 157 358 000 60
+        </div>
+
+        <div class="links">
+            <a href="/{{ request()->input('d') ? '?d=' . request()->input('d') : '' }}">Back</a>
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Back soon.</title>
+        @include('styles')
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+
+                <div class="title m-b-md">
+                    Back soon.
+                </div>
+
+                <div class="m-b-md--small">
+                    <strong>Inspiring quote here...</strong> (Please imagine now.)
+                </div>
+
+                <div class="links">
+                    <a href="/imprint">Imprint</a>
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
