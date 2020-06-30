13b1c8727e2185c630709f93c1f4d026124bf38b
Daniel (TMP) Seuffer <authanram@gmail.com>
2020-06-30 07:12:40 +0200

    WIP

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -4,11 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 class DomainController extends Controller
 {
     final public function index(string $domain): RedirectResponse
     {
+        if (false === \strpos($domain, '.')) {
+
+            abort(404);
+
+        }
+
+        $validator = Validator::make(['domain' => "http://$domain"], [
+
+            'domain' => 'nullable|url',
+
+        ]);
+
+        if ($validator->fails()) {
+
+            abort(404);
+
+        }
+
         Session::put('domain', $domain);
 
         return redirect('/');
